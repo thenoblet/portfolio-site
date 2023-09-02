61fe9332ba1deaa8a34c38668b16b8005a35eb61
noblet <appiahpatricknoblet@gmail.com>
2023-09-02 20:20:31 +0000

    Sat, Sep 2, 2023, 8:20 PM +00:00

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ function toggleMenu() {
         icon.classList.add("open");
     }
 }
+
+// Add an event listener to close the menu when a list item is clicked
+const menuLinks = document.querySelectorAll(".menu-links a");
+
+menuLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        const menu = document.querySelector(".menu-links");
+        const icon = document.querySelector(".hamburger-icon");
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    });
+});
+
+// Close the menu when clicking anywhere outside of it
+document.addEventListener("click", (event) => {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    if (!menu.contains(event.target) && !icon.contains(event.target)) {
+        menu.classList.remove("open");
+        icon.classList.remove("open");
+    }
+});
